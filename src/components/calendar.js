9c5c5b32489d61ac05886bb833b3a9271d0d3772
AgataMaria <agata.karzarnowicz@gmail.com>
2020-11-08 12:23:10 +0000

    passing over to Fabio

---
+++ src/components/calendar.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Days from "../days.json"
+
+const Calendar = () => (
+    <div className="calendar">
+        {console.log(Days)}
+        {/* {Days.map((day, message) => {
+            {console.log(day)}
+        })} */}
+    </div>
+
+)
+
+export default Calendar
